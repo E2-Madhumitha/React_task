0ca8c7b458ce24e6155d3fd92fe6764528909bd8
E2-Madhumitha <88490983+E2-Madhumitha@users.noreply.github.com>
2022-11-05 12:17:21 +0530

    Update validationcontrol.js

--- validationcontrol.js
+++ validationcontrol.js
@@ -11,7 +11,64 @@ function Validationcontrol() {
     const handlechange = (e) => {
         const name = e.target.name;
         const value = e.target.value;
-        seterror(validate(details));
+        const re = /^[A-Za-z]*$/;
+        const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        const num = /^[0-9\b]+$/;
+        const pwd =
+            /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-_]).{8,}$/;
+
+        const removeEmptySpaces = /^\s/g;
+        if (name == "username" && !value) {
+            seterror({ ...error, username: "Username is required" });
+        } else if (
+            name == "username" &&
+            (removeEmptySpaces.test(value) || !re.test(value))
+        ) {
+            seterror({ ...error, username: "Only alphabets allowed" });
+        } else if (name == "username" && re.test(value)) {
+            seterror({ ...error, username: "" });
+        }
+
+        if (name == "mobilenumber" && !value) {
+            seterror({ ...error, mobilenumber: "Mobilenumber is required" });
+        } else if (
+            name == "mobilenumber" &&
+            (removeEmptySpaces.test(value) || !num.test(value))
+        ) {
+            seterror({ ...error, mobilenumber: "Only numerics are allowed" });
+        } else if (
+            name == "mobilenumber" &&
+            (value.length < 9 || value.length > 10)
+        ) {
+            seterror({
+                ...error,
+                mobilenumber: "Mobilenumber must contain 10 digits",
+            });
+        } else if (name == "mobilenumber" && num.test(value)) {
+            seterror({ ...error, mobilenumber: "" });
+        }
+
+        if (name == "email" && !value) {
+            seterror({ ...error, email: "Email is required" });
+        } else if (name == "email" && !regex.test(value)) {
+            seterror({ ...error, email: "This is not valid" });
+        } else if (name == "email" && regex.test(value)) {
+            seterror({ ...error, email: "" });
+        }
+
+        if (name == "password" && !value) {
+            seterror({ ...error, password: "Password is required" });
+        } else if (name == "password" && value.length > 15) {
+            seterror({
+                ...error,
+                password: "Password must be less than 15 characters",
+            });
+        } else if (name == "password" && !pwd.test(value)) {
+            seterror({ ...error, password: "This is not valid" });
+        } else if (name == "password" && pwd.test(value)) {
+            seterror({ ...error, password: "" });
+        }
+
         setdetails((prev) => {
             return { ...prev, [name]: value };
         });
@@ -42,69 +99,28 @@ function Validationcontrol() {
         ) {
             submitted(true);
         } else if (
-            !details.username &&
-            !details.mobilenumber &&
-            !details.email &&
+            !details.username ||
+            !details.mobilenumber ||
+            !details.email ||
             !details.password
         ) {
             seterror(val(details));
         }
-        localStorage.setItem("details", JSON.stringify(details));
+        // localStorage.setItem("details", JSON.stringify(details));
     };
 
-    useEffect(() => {
-        const data = localStorage.getItem("details");
-        console.log(data);
-        if (data) {
-            setdetails(JSON.parse(data));
-        }
-    }, []);
-
-    const validate = (values) => {
-        const errors = {};
-        const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        const removeEmptySpaces = /^\s/g;
-        const re = /^[A-Za-z]*$/;
-        const num = /^[0-9\b]+$/;
-        const pwd =
-            /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
-        if (!values.username) {
-            errors.username = "Username is required";
-        } else if (
-            removeEmptySpaces.test(values.username) ||
-            !re.test(values.username)
-        ) {
-            errors.username = "Only alphabets allowed";
-        }
-        if (!values.mobilenumber) {
-            errors.mobilenumber = "Mobilenumber is required";
-        } else if (
-            removeEmptySpaces.test(values.mobilenumber) ||
-            !num.test(values.mobilenumber)
-        ) {
-            errors.mobilenumber = "Only numerics are allowed";
-        } else if (
-            values.mobilenumber.length < 9 ||
-            values.mobilenumber.length > 10
-        ) {
-            errors.mobilenumber = "Mobilenumber must contain 10 digits";
-        }
-        if (!values.email) {
-            errors.email = "Email is required";
-        } else if (!regex.test(values.email)) {
-            errors.email = "This is not valid";
-        }
-
-        if (!values.password) {
-            errors.password = "Password is required";
-        } else if (values.password.length > 15) {
-            errors.password = "Password must be less than 15 characters";
-        } else if (!pwd.test(values.password)) {
-            errors.password = "This is not valid";
-        }
-        return errors;
-    };
-
+    // useEffect(() => {
+    //     const data = localStorage.getItem("details");
+    //     console.log(data);
+    //     if (data) {
+    //         setdetails(JSON.parse(data));
+    //     }
+    // }, []);
+    console.log(Object.keys(error).length);
+    console.log(error.username);
+    console.log(error.mobilenumber);
+    console.log(error.email);
+    console.log(error.password);
     return (
         <div className="container">
             <form onSubmit={submitchange}>
@@ -114,7 +130,7 @@ function Validationcontrol() {
                     </div>
                     <div className="content">
                         <div className="name">
-                            <label>Username</label>
+                            <label>Username*</label>
                             <input
                                 type="text"
                                 name="username"
@@ -124,7 +140,7 @@ function Validationcontrol() {
                             <span>{error.username}</span>
                         </div>
                         <div className="mobilenumber">
-                            <label>Mobilenumber</label>
+                            <label>Mobilenumber*</label>
                             <input
                                 type="text"
                                 name="mobilenumber"
@@ -134,7 +150,7 @@ function Validationcontrol() {
                             <span>{error.mobilenumber}</span>
                         </div>
                         <div className="email">
-                            <label>Email</label>
+                            <label>Email*</label>
                             <input
                                 type="text"
                                 name="email"
@@ -144,7 +160,7 @@ function Validationcontrol() {
                             <span>{error.email}</span>
                         </div>
                         <div className="password">
-                            <label>Password</label>
+                            <label>Password*</label>
                             <input
                                 type="text"
                                 name="password"
@@ -156,8 +172,12 @@ function Validationcontrol() {
                         <div className="but">
                             <button type="submit">Submit</button>
                             <p style={{ color: "green" }}>
-                                {issubmit && Object.keys(error).length === 0
-                                    ? "successfully validated!!!"
+                                {issubmit &&
+                                error.username.length == 0 &&
+                                error.mobilenumber.length == 0 &&
+                                error.email.length == 0 &&
+                                error.password.length == 0
+                                    ? "SUCCESSFULLY VALIDATED!!!"
                                     : null}
                             </p>
                         </div>
@@ -169,4 +189,3 @@ function Validationcontrol() {
 }
 
 export default Validationcontrol;
-
