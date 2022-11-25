70c57596748b5e60210a47e8f4d91886a434d8d8
E2-Madhumitha <you@example.com>
2022-11-25 13:57:56 +0530

    scss loader

---
+++ react-app scss/src/componentsv5/Contact.js
@@ -0,0 +1,26 @@
+import { useHistory, useLocation } from "react-router-dom";
+import React from "react";
+function Contact() {
+    const location = useLocation();
+    const history = useHistory();
+    console.log(location);
+    console.log(history);
+    const gobackHandle = () => {
+        history.goBack();
+        // history.push("/");
+    };
+    const goforwardHandle = () => {
+        history.goForward();
+        // history.push("/");
+    };
+    return (
+        <>
+            <p>CONTACT PAGE</p>
+            <p>Location:{location.pathname}</p>
+            <p>From:{location.state.from}</p>
+            <button onClick={gobackHandle}>BACK</button>
+            <button onClick={goforwardHandle}>FORWARD</button>
+        </>
+    );
+}
+export default Contact;
