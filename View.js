e80369db54fe4b40eac251b09e8d3173d38b8cc7
E2-Madhumitha <88490983+E2-Madhumitha@users.noreply.github.com>
2022-12-07 17:51:41 +0530

    Update View.js

--- View.js
+++ View.js
@@ -1,17 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { connect, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-
+import { useState } from "react";
 export const View = (props) => {
     const navigate = useNavigate();
     // const data = JSON.parse(localStorage.getItem("details"));
     // console.log(data);
     const inputarr = useSelector((state) => state.inputarr);
+    const [searchItem, setSearchItem] = useState("");
     return (
         <div className="view-container">
             {
                 <div className="tablecontainer">
-                    <input type="search" placeholder="search" />
+                    <input
+                        type="text"
+                        placeholder="search"
+                        onChange={(e) => {
+                            setSearchItem(e.target.value);
+                        }}
+                    />
                     <table border={1} className="table">
                         <thead>
                             <tr>
@@ -29,17 +36,53 @@ export const View = (props) => {
                                     <td colSpan={4}>NO data Enter yet !</td>
                                 </tr>
                             ) : (
-                                inputarr.map((info, ind) => {
-                                    return (
-                                        <tr key={ind}>
-                                            <td>{info.name}</td>
-                                            <td>{info.department}</td>
-                                            <td>{info.job_title}</td>
-                                            <td>{info.contact_number}</td>
-                                            <td>{info.years_of_experience}</td>
-                                        </tr>
-                                    );
-                                })
+                                inputarr
+                                    .filter((val) => {
+                                        if (searchItem === "") {
+                                            return val;
+                                        } else if (
+                                            val.name
+                                                .toLowerCase()
+                                                .includes(
+                                                    searchItem.toLowerCase()
+                                                ) ||
+                                            val.department
+                                                .toLowerCase()
+                                                .includes(
+                                                    searchItem.toLowerCase()
+                                                ) ||
+                                            val.job_title
+                                                .toLowerCase()
+                                                .includes(
+                                                    searchItem.toLowerCase()
+                                                ) ||
+                                            val.contact_number
+                                                .toLowerCase()
+                                                .includes(
+                                                    searchItem.toLowerCase()
+                                                ) ||
+                                            val.years_of_experience
+                                                .toLowerCase()
+                                                .includes(
+                                                    searchItem.toLowerCase()
+                                                )
+                                        ) {
+                                            return val;
+                                        }
+                                    })
+                                    .map((info, ind) => {
+                                        return (
+                                            <tr key={ind}>
+                                                <td>{info.name}</td>
+                                                <td>{info.department}</td>
+                                                <td>{info.job_title}</td>
+                                                <td>{info.contact_number}</td>
+                                                <td>
+                                                    {info.years_of_experience}
+                                                </td>
+                                            </tr>
+                                        );
+                                    })
                             )}
                         </tbody>
                     </table>
@@ -56,3 +99,4 @@ export const View = (props) => {
 // };
 export default View;
 //connect(mapStateToProps)(View);
+
